558c95f2de28d75c84961c2b55e7e56d2ce16742
Vasilis Christou <160491613+vchr1stou@users.noreply.github.com>
2025-05-14 18:43:42 +0300

    started streak screen

--- lib/presentation/streak_screen/streak_screen.dart
+++ lib/presentation/streak_screen/streak_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:calendar_date_picker2/calendar_date_picker2.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import '../../core/app_export.dart';
 import '../../theme/custom_button_style.dart';
 import '../../widgets/app_bar/appbar_trailing_iconbutton.dart';
@@ -29,214 +31,237 @@ class StreakScreenState extends State<StreakScreen> {
   @override
   void initState() {
     super.initState();
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        systemNavigationBarColor: Colors.transparent,
+        systemNavigationBarDividerColor: Colors.transparent,
+      ),
+    );
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.edgeToEdge,
+      overlays: [SystemUiOverlay.top],
+    );
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.manual,
+      overlays: SystemUiOverlay.values,
+    );
+    super.dispose();
+  }
+
+  void _handleCloseButton() {
+    Navigator.of(context).pop();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBody: true,
-      extendBodyBehindAppBar: true,
-      backgroundColor: appTheme.gray6004c,
-      body: Container(
-        width: double.maxFinite,
-        height: SizeUtils.height,
-        decoration: AppDecoration.gradientAmberToRed400,
-        child: SafeArea(
-          child: SizedBox(
-            width: double.maxFinite,
-            child: SingleChildScrollView(
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        extendBody: true,
+        extendBodyBehindAppBar: true,
+        body: Stack(
+          children: [
+            // Background Image
+            Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/background.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            // Semi-transparent overlay with blur
+            Positioned.fill(
+              child: ClipRect(
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                  child: Container(
+                    color: Color(0xFF000000).withOpacity(0.15),
+                  ),
+                ),
+              ),
+            ),
+            // Close Button
+            Positioned(
+              top: 62.h,
+              right: 15.h,
               child: Container(
-                width: double.maxFinite,
-                padding: EdgeInsets.only(top: 26.h),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      width: double.maxFinite,
-                      child: _buildAppbar(context),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      blurRadius: 35,
+                      spreadRadius: 2,
                     ),
-                    CustomImageView(
-                      imagePath: ImageConstant.imgVectorOrange400,
-                      height: 142.h,
-                      width: 110.h,
+                  ],
+                ),
+                child: GestureDetector(
+                  onTap: _handleCloseButton,
+                  child: SvgPicture.asset(
+                    'assets/images/close_button.svg',
+                    width: 39.h,
+                    height: 39.h,
+                  ),
+                ),
+              ),
+            ),
+            // Streak Flame - 29 pixels down from close button, centered
+            Positioned(
+              top: 130.h, // 62 + 39 + 29 = 130
+              left: 0,
+              right: 0,
+              child: Column(
+                children: [
+                  Center(
+                    child: SvgPicture.asset(
+                      'assets/images/flame.svg',
+                      width: 112.h,
+                      height: 154.h,
                     ),
-                    SizedBox(height: 10.h),
-                    SizedBox(
-                      width: double.maxFinite,
-                      child: RichText(
-                        text: TextSpan(
+                  ),
+                  SizedBox(height: 10.h),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 30.h),
+                    child: Text(
+                      "A streak is your mood-tracking winning spree! 🤩\nEach day you log your mood, you keep the chain alive,\nthink of it as collecting little victories for your mind!",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.roboto(
+                        color: Colors.white,
+                        fontSize: 13.1,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15.h),
+                  // Streak Bubbles
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      // Left bubble
+                      Container(
+                        width: 157.h,
+                        height: 67.h,
+                        child: Stack(
                           children: [
-                            TextSpan(
-                              text: "A streak is your",
-                              style: CustomTextStyles.titleSmallRoboto14,
+                            SvgPicture.asset(
+                              'assets/images/bubble_streak.svg',
+                              width: 157.h,
+                              height: 67.h,
                             ),
-                            TextSpan(
-                              text: "Each day you",
-                              style: CustomTextStyles.titleMediumOnPrimary,
+                            Positioned(
+                              top: 8.h,
+                              left: 0,
+                              right: 0,
+                              child: Column(
+                                children: [
+                                  Text(
+                                    "Longest Streak",
+                                    textAlign: TextAlign.center,
+                                    style: GoogleFonts.roboto(
+                                      color: Colors.white,
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5.h),
+                                  Text(
+                                    "20 days",
+                                    textAlign: TextAlign.center,
+                                    style: GoogleFonts.roboto(
+                                      color: Colors.white,
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ],
                         ),
-                        textAlign: TextAlign.center,
-                        maxLines: 4,
-                        overflow: TextOverflow.ellipsis,
                       ),
-                    ),
-                    SizedBox(height: 12.h),
-                    _buildRowLongestStreak(context),
-                    SizedBox(height: 16.h),
-                    CustomOutlinedButton(
-                      height: 50.h,
-                      text: "Quick 7 hours",
-                      margin: EdgeInsets.symmetric(horizontal: 30.h),
-                      buttonStyle: CustomButtonStyles.none,
-                      decoration: CustomButtonStyles.outlineTL24Decoration,
-                      buttonTextStyle: CustomTextStyles.labelLargeRoboto,
-                      hasBlurBackground: true,
-                    ),
-                    SizedBox(height: 18.h),
-                    _buildCalendarOne(context),
-                    SizedBox(height: 42.h),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  PreferredSizeWidget _buildAppbar(BuildContext context) {
-    return CustomAppBar(
-      height: 30.h,
-      actions: [
-        AppbarTrailingIconbutton(
-          imagePath: ImageConstant.imgClose,
-          margin: EdgeInsets.only(right: 24.h),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildRowLongestStreak(BuildContext context) {
-    return Container(
-      width: double.maxFinite,
-      margin: EdgeInsets.symmetric(horizontal: 30.h),
-      child: Row(
-        children: [
-          Expanded(
-            child: ClipRect(
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 100, sigmaY: 100),
-                child: Container(
-                  width: double.maxFinite,
-                  padding: EdgeInsets.symmetric(vertical: 10.h),
-                  decoration: AppDecoration.windowsGlassBlur.copyWith(
-                      borderRadius: BorderRadiusStyle.roundedBorder32),
-                  child: Column(
-                    spacing: 6,
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Longest streak",
-                        style: CustomTextStyles.labelLargeRoboto,
-                      ),
-                      Text(
-                        "152 days",
-                        style: CustomTextStyles.labelLargeRobotoMedium,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: ClipRect(
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 100, sigmaY: 100),
-                child: Container(
-                  width: double.maxFinite,
-                  padding: EdgeInsets.only(left: 30.h, top: 8.h, bottom: 8.h),
-                  decoration: AppDecoration.windowsGlassBlur.copyWith(
-                      borderRadius: BorderRadiusStyle.roundedBorder32),
-                  child: Column(
-                    spacing: 6,
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Current streak",
-                        style: CustomTextStyles.labelLargeRoboto,
-                      ),
-                      Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          "7 days",
-                          style: CustomTextStyles.labelLargeRobotoMedium,
+                      // Right bubble
+                      Container(
+                        width: 157.h,
+                        height: 67.h,
+                        child: Stack(
+                          children: [
+                            SvgPicture.asset(
+                              'assets/images/bubble_streak.svg',
+                              width: 157.h,
+                              height: 67.h,
+                            ),
+                            Positioned(
+                              top: 8.h,
+                              left: 0,
+                              right: 0,
+                              child: Column(
+                                children: [
+                                  Text(
+                                    "Current Streak",
+                                    textAlign: TextAlign.center,
+                                    style: GoogleFonts.roboto(
+                                      color: Colors.white,
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5.h),
+                                  Text(
+                                    "20 days",
+                                    textAlign: TextAlign.center,
+                                    style: GoogleFonts.roboto(
+                                      color: Colors.white,
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildCalendarOne(BuildContext context) {
-    return SizedBox(
-      width: double.maxFinite,
-      child: Consumer<StreakProvider>(
-        builder: (context, provider, child) {
-          return Container(
-            height: 314.h,
-            width: 354.h,
-            margin: EdgeInsets.symmetric(horizontal: 24.h),
-            child: CalendarDatePicker2(
-              config: CalendarDatePicker2Config(
-                calendarType: CalendarDatePicker2Type.multi,
-                firstDate: DateTime(DateTime.now().year - 5),
-                lastDate: DateTime(DateTime.now().year + 5),
-                selectedDayHighlightColor: Color(0X99FF9500),
-                firstDayOfWeek: 0,
-                weekdayLabelTextStyle: TextStyle(
-                  color: appTheme.gray700,
-                  fontFamily: 'SF Pro',
-                  fontWeight: FontWeight.w600,
-                ),
-                selectedDayTextStyle: TextStyle(
-                  color: theme.colorScheme.onPrimary,
-                  fontFamily: 'SF Pro',
-                  fontWeight: FontWeight.w400,
-                ),
-                dayTextStyle: TextStyle(
-                  color: theme.colorScheme.onPrimary,
-                  fontFamily: 'SF Pro',
-                  fontWeight: FontWeight.w400,
-                ),
-                weekdayLabels: [
-                  'sun',
-                  'mon',
-                  'tue',
-                  'wed',
-                  'thu',
-                  'fri',
-                  'sat'
+                  // Alert streak
+                  SizedBox(height: 15.h), // 10 pixels down from bubbles
+                  Center(
+                    child: Container(
+                      width: 340.h,
+                      height: 51.h,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          SvgPicture.asset(
+                            'assets/images/alert_streak.svg',
+                            width: 340.h,
+                            height: 51.h,
+                          ),
+                          Text(
+                            "⏰ Quick! 7 hours left to save your streak!",
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.roboto(
+                              color: Colors.white,
+                              fontSize: 13,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
-                dayBorderRadius: BorderRadius.circular(15.h),
               ),
-              value: provider.selectedDatesFromCalendar ?? [],
-              onValueChanged: (dates) {
-                provider.selectedDatesFromCalendar = dates;
-              },
             ),
-          );
-        },
+          ],
+        ),
       ),
     );
   }
