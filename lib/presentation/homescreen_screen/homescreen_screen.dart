868340351a822b59b37723a24d82e4b2c64cf5a9
Vasilis Christou <160491613+vchr1stou@users.noreply.github.com>
2025-05-17 14:19:30 +0300

    sos function from 1 minute to 10 seconds

--- lib/presentation/homescreen_screen/homescreen_screen.dart
+++ lib/presentation/homescreen_screen/homescreen_screen.dart
@@ -754,7 +754,7 @@ class HomescreenScreenState extends State<HomescreenScreen> with AutomaticKeepAl
                           MaterialPageRoute(
                             builder: (context) => InhaleScreen.builder(context),
                             settings: RouteSettings(
-                              arguments: {'duration': 1},
+                              arguments: {'duration': 0.166}, // 10 seconds (10/60 minutes)
                             ),
                           ),
                         );
